cd49c04b394e6dbbdaa95eb459b521ad0fcdf4f1
Pablo Pesce <papesce@gmail.com>
2018-01-17 12:22:11 -0300

    add mutation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+
 
 const resolvers = {
-  Query,
+  Query, Mutation
 }
 // const resolvers = {
 //   Query: {
